50f69f23f23041f55a3cd7b9af8da4362b796d54
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-07-05 22:41:00 +0200

    Add test cases

--- spec/tiny_factory_spec.rb
+++ spec/tiny_factory_spec.rb
@@ -3,7 +3,49 @@ RSpec.describe TinyFactory do
     expect(TinyFactory::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  before do
+    described_class.define :user do
+      first_name { "John" }
+      last_name { "Doe" }
+      email { "#{first_name}.#{last_name}@hey.com".downcase }
+    end
+  end
+
+  describe ".attributes_for" do
+    subject(:attributes) { described_class.attributes_for(:user) }
+
+    it "creates a hash of attributes" do
+      expect(attributes).to be_kind_of(Hash)
+
+      expect(attributes[:first_name]).to eq("John")
+      expect(attributes[:last_name]).to eq("Doe")
+      expect(attributes[:email]).to eq("john.doe@hey.com")
+    end
+  end
+
+  describe ".build" do
+    subject(:user) { described_class.build(:user) }
+
+    it "builds a user" do
+      expect(:user).to be_kind_of(User)
+      expect(:user.new_record?).to be_truthy
+
+      expect(user.first_name).to eq("John")
+      expect(user.last_name).to eq("Doe")
+      expect(user.email).to eq("john.doe@hey.com")
+    end
+  end
+
+  describe ".create" do
+    subject(:user) { described_class.create(:user) }
+
+    it "creates a persisted user" do
+      expect(:user).to be_kind_of(User)
+      expect(:user.persisted?).to be_truthy
+
+      expect(user.first_name).to eq("John")
+      expect(user.last_name).to eq("Doe")
+      expect(user.email).to eq("john.doe@hey.com")
+    end
   end
 end
