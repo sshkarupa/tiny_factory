82aa81b618628d3aa87c262c06fed8f8a9160a74
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-07-06 10:24:33 +0200

    Add Attribute class

---
+++ lib/tiny_factory/attribute.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module TinyFactory
+  class Attribute
+    attr_reader :name, :definition
+
+    def initialize(name, definition)
+      @name = name.to_sym
+      @definition = definition
+    end
+
+    def add_to(strategy)
+      strategy.set(name, strategy.instance_eval(&definition))
+    end
+  end
+end
