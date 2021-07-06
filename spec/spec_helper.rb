0a89349812df59e06deb552a9b4545c879a00d28
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-07-06 10:36:09 +0200

    Use syntactic sugar for calling TinyFactory methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include TinyFactory::Syntax::Methods
 end
 
 ActiveRecord::Base.establish_connection(
